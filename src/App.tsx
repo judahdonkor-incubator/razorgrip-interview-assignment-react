fa41d5a1d396bbf492165f56244705c1527cf3ff
judahdonkor <judahdonkor@gmail.com>
2021-06-06 23:51:31 +0000

    Patch notification

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ import { UsersOnline, UserCard, User } from './user'
 import axios from 'axios'
 import useWebSocket, { ReadyState } from 'react-use-websocket'
 import { Messenger, Messages, Message } from "./chat"
+import Notification from 'react-web-notification'
 
 type Data = {
   message: 'users-online' | 'blocked-users' | 'user-online' | 'user-offline' | 'user-blocked' | 'user-unblocked' | 'new-message'
@@ -73,9 +74,11 @@ export const App = () => {
             .filter(({ senderId }) => recipient !== senderId))
         })
   }, [recipient])
+  const [notification, setNotification] = useState<Record<'title' | 'body', string> | undefined>()
   // fix hard-coded url later
-  const toast = useToast()
   useWebSocket(`wss://razorgrip-interview-assignment.herokuapp.com?token=${user?.sub}`, {
+    shouldReconnect: e => true,
+    retryOnError: true,
     onMessage: e => {
       const msg: Data = JSON.parse(e.data)
       switch (msg.message) {
@@ -116,11 +119,9 @@ export const App = () => {
               })
           } else {
             if (msg.data.recipientId === user?.sub)
-              toast({
-                title: usersOnline.find(({ sub }) => msg.data.senderId === sub)?.name || msg.data.senderId,
-                description: msg.data.message,
-                status: 'info',
-                isClosable: true,
+              setNotification({
+                body: msg.data.message,
+                title: usersOnline.find(({ sub }) => msg.data.senderId === sub)?.name || msg.data.senderId
               })
             setNewMessages(arr => [...arr, msg.data])
           }
@@ -132,69 +133,81 @@ export const App = () => {
     }
   }, !!user)
   return (
-    <ChakraProvider theme={theme}>
-      {user && (
-        <Flex h="100vh">
-          <VStack
-            align="stretch"
-          >
-            <Flex bg='gray.400'>
-              <Box p="4">
-                <UserCard user={user} />
-              </Box>
-              <Spacer />
-              <Box p="4">
-                <ColorModeSwitcher justifySelf="flex-end" />
-              </Box>
-            </Flex>
-            <UsersOnline
-              users={usersOnline}
-              blocked={blockedUsers.map(({ recipientId }) => recipientId)}
-              recipient={recipient}
-              setRecipient={setRecipient}
-              usersWithNewMessages={newMessages.map(({ senderId }) => senderId)} />
-          </VStack>
-          <Divider orientation="vertical" borderColor="gray.200" h='100vh' />
-          <Flex flex="1" direction='column' >
-            {recipient !== undefined ? (
-              <>
-                <Flex bg='gray.400'>
-                  <Box p="4">
-                    <UserCard user={usersOnline.find(({ sub }) => sub === recipient)!} />
-                  </Box>
-                  <Spacer />
-                  <Box p="4">
-                    <Button
-                      variant="outline"
-                      aria-label='Block user'
-                      onClick={() => (axios.request({
-                        method: blockedUsers.map(({ senderId }) => senderId).includes(recipient) ? 'DELETE' : 'PATCH',
-                        url: `/api/user/blocked/${recipient}`
-                      }))}>
-                      {blockedUsers.map(({ senderId }) => senderId).includes(recipient) ? 'Unblock' : 'Block'}
-                    </Button>
-                  </Box>
-                </Flex>
-                <Box flex="1" overflow='scroll'>
-                  <Messages
-                    data={messages[recipient]}
-                    recipient={recipient} />
+    <>
+      <ChakraProvider theme={theme}>
+        {user && (
+          <Flex h="100vh">
+            <VStack
+              align="stretch"
+            >
+              <Flex bg='gray.400'>
+                <Box p="4">
+                  <UserCard user={user} />
                 </Box>
-                <Messenger recipient={recipient} />
-              </>
-            ) : (
-              <Center h="full">
-                <VStack textAlign='center'>
-                  <Image src='https://static.wixstatic.com/media/629476_7855f7e6a96045ebbad4f3f993ee336f~mv2.png/v1/fill/w_220,h_60,al_c,q_85,usm_0.66_1.00_0.01/razorgrip-logo.webp' />
-                  <Heading>
-                    Live instant messaging pplication
+                <Spacer />
+                <Box p="4">
+                  <ColorModeSwitcher justifySelf="flex-end" />
+                </Box>
+              </Flex>
+              <UsersOnline
+                users={usersOnline}
+                blocked={blockedUsers.map(({ recipientId }) => recipientId)}
+                recipient={recipient}
+                setRecipient={setRecipient}
+                usersWithNewMessages={newMessages.map(({ senderId }) => senderId)} />
+            </VStack>
+            <Divider orientation="vertical" borderColor="gray.200" h='100vh' />
+            <Flex flex="1" direction='column' >
+              {recipient !== undefined ? (
+                <>
+                  <Flex bg='gray.400'>
+                    <Box p="4">
+                      <UserCard user={usersOnline.find(({ sub }) => sub === recipient)!} />
+                    </Box>
+                    <Spacer />
+                    <Box p="4">
+                      <Button
+                        variant="outline"
+                        aria-label='Block user'
+                        onClick={() => (axios.request({
+                          method: blockedUsers.map(({ senderId }) => senderId).includes(recipient) ? 'DELETE' : 'PATCH',
+                          url: `/api/user/blocked/${recipient}`
+                        }))}>
+                        {blockedUsers.map(({ senderId }) => senderId).includes(recipient) ? 'Unblock' : 'Block'}
+                      </Button>
+                    </Box>
+                  </Flex>
+                  <Box flex="1" overflow='scroll'>
+                    <Messages
+                      data={messages[recipient]}
+                      recipient={recipient} />
+                  </Box>
+                  <Messenger recipient={recipient} />
+                </>
+              ) : (
+                <Center h="full">
+                  <VStack textAlign='center'>
+                    <Image src='https://static.wixstatic.com/media/629476_7855f7e6a96045ebbad4f3f993ee336f~mv2.png/v1/fill/w_220,h_60,al_c,q_85,usm_0.66_1.00_0.01/razorgrip-logo.webp' />
+                    <Heading>
+                      Live instant messaging pplication
                   </Heading>
-                </VStack>
-              </Center>
-            )}
+                  </VStack>
+                </Center>
+              )}
+            </Flex>
           </Flex>
-        </Flex>
-      )}
-    </ChakraProvider>
+        )}
+      </ChakraProvider>
+      {notification ? (
+        <Notification
+          title={notification.title}
+          options={{
+            icon: "http://mobilusoss.github.io/react-web-notification/example/Notifications_button_24.png",
+            body: notification.body
+          }}
+          onClose={() => setNotification(undefined)}
+        />
+      ) : null}
+    </>
   )
 }
